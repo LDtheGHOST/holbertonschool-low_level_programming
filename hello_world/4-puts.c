461b084493492d530510035f942e190fe0271cd7
LDtheGOHST <LDtheGOHST.luis-david.cuevas@epitech.eu>
2024-02-21 02:28:37 -0800

    ex.1

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,1 +1,11 @@
-ksdcbnsdo
+#include <stdio.h>
+/*
+  main - Entry point
+
+  Return: Always 0
+ */
+int main(void)
+{
+		puts("Programming is like building a multilingual puzzle");
+			return (0);
+}
