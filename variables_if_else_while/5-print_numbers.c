fbff9406d3eeeacb37ce24dff463c03352c112f5
LDtheGOHST <LDtheGOHST.luis-david.cuevas@epitech.eu>
2024-02-25 20:43:47 +0000

    ex.2

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+nclude <stdio.h>
+
+/**
+ * main - prints all single digit numbers:wq
+ *
+ * Return: Always 0.
+ **/
+
+int main(void)
+{
+int num;
+
+for (num = 0; num < 10; num++)
+printf("%d", num);
+
+printf("\n");
+
+return (0);
+}
