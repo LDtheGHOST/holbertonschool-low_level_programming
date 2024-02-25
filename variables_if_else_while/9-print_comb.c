857804cafdfa8ee7c7261a7add42909d8bfaa65e
LDtheGOHST <LDtheGOHST.luis-david.cuevas@epitech.eu>
2024-02-25 20:56:09 +0000

    ex.2

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -5,19 +5,19 @@
  * Return: Always 0.
  **/
 int main(void)
-{		
-int num;
 {
-for (num = 0; num <= 9; num++)
-{
-putchar(num + '0');
-if (num < 9)
-{
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
-return (0);
-}
+	int num;
+	{
+		for (num = 0; num <= 9; num++)
+		{
+			putchar(num + '0');
+			if (num < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+		return (0);
+	}
 }
