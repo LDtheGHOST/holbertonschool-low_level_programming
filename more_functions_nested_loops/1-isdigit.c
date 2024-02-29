7ac07461f5e3fe30378da3b84312b85597d4c881
luis-david <luis-david.cuevas@epitech.eu>
2024-02-29 11:31:10 +0100

    27 fevirer

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -4,7 +4,7 @@
  * @c: int nombre entier
  * Return: 1 si c est un chiffre sinon 0
  **/
-int _isdigit(int c)
+int _isdigit(int c) // int nombre entier de la function _isdigit
 {
-return (c >= '0' && c <= '9');
+return (c >= '0' && c <= '9'); // && et 
 }
---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul -  funtion qui mutiplie deux entier
+ * @a: premier numero
+ * @b: deuxieme numero
+ * Return: result resultat.
+ **/
+int mul(int a, int b)
+{
+		return (a * b);
+}
