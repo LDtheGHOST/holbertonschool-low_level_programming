69246259e96d7277b25116af0d3b2f8251cd1640
luis-david <luis-david.cuevas@epitech.eu>
2024-03-01 16:11:58 +0100

    27 fevirer

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -4,17 +4,17 @@
  * print_line - draws a straight line in the terminal
  * @n: number of times the character _ should be printed
  **/
-void print_line(int n)
+void print_line(int n) // function
 {
-if (n <= 0)
+if (n <= 0) // si n = 0
 {
-_putchar('\n');
+_putchar('\n'); // un carectere
 }
 else
 {
 int a;
 
-for (a = 1; a <= n; a++)
+for (a = 1; a <= n; a++) // condition 
 {
 _putchar('_');
 }
---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagonal -dessine une ligne diagonale sur le terminal
+ * @n: nomber de fois ou le caractere \ doit etre imprime
+ **/
+void print_diagonal(int n) // function
+{
+if (n <= 0) //si
+{
+_putchar('\n'); // un seul caractere
+}
+else
+{
+int i, j;
+
+for (i = 0; i < n; i++) // pour, condition
+{
+for (j = 0; j < n; j++)
+{
+if (j == i)
+_putchar('\\'); //un caractaire
+else if (j < i)
+_putchar(' ');
+}
+_putchar('\n');
+}
+}
+}
