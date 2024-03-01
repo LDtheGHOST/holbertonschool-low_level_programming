3f849894b6d4b7896e6f15d48b71c27d71cdcbfe
luis-david <luis-david.cuevas@epitech.eu>
2024-03-01 16:14:49 +0100

    27 fevirer

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -4,22 +4,22 @@
  * print_diagonal -dessine une ligne diagonale sur le terminal
  * @n: nomber de fois ou le caractere \ doit etre imprime
  **/
-void print_diagonal(int n) // function
+void print_diagonal(int n)
 {
-if (n <= 0) //si
+if (n <= 0)
 {
-_putchar('\n'); // un seul caractere
+_putchar('\n');
 }
 else
 {
 int i, j;
 
-for (i = 0; i < n; i++) // pour, condition
+for (i = 0; i < n; i++)
 {
 for (j = 0; j < n; j++)
 {
 if (j == i)
-_putchar('\\'); //un caractaire
+_putchar('\\');
 else if (j < i)
 _putchar(' ');
 }
