c82cae7cacd1c8d3a49783373d811058fc5dd828
luis-david <luis-david.cuevas@epitech.eu>
2024-03-01 11:48:48 +0100

    27 fevirer

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -4,17 +4,17 @@
  * more_numbers -  imprime 10 fois les nombres, de 0 a 14
  * suivi d'une new line.
  **/
-void more_numbers(void)
+void more_numbers(void) // function
 {
-int i;
+int i; // nombre entier
 int num = 0;
 
-for (i = 0 ; i < 10 ; i++)
+for (i = 0 ; i < 10 ; i++) // condition 
 {
 while (num < 15)
 {
-if (num > 9)
-_putchar('0' + num / 10);
+if (num > 9) // si
+_putchar('0' + num / 10); // un seul caratere
 _putchar('0' + num % 10);
 num++;
 }
