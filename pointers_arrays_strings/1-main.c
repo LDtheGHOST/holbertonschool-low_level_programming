bfa84a93c214fc02e924ee5034178cb6dcc152b0
luis-david <luis-david.cuevas@epitech.eu>
2024-03-03 20:32:10 +0100

    29 fevirer

--- pointers_arrays_strings/1-main.c
+++ pointers_arrays_strings/1-main.c
@@ -18,3 +18,22 @@ swap_int(&a, &b);
 printf("a=%d, b=%d\n", a, b);
 return (0);
 }
+
+
+{
+char s1[98] = "Hello ";
+char s2[] = "World!\n";
+char *ptr;
+
+printf("%s\n", s1);
+printf("%s", s2);
+ptr = _strncat(s1, s2, 1);
+printf("%s\n", s1);
+printf("%s", s2);
+printf("%s\n", ptr);
+ptr = _strncat(s1, s2, 1024);
+printf("%s", s1);
+printf("%s", s2);
+printf("%s", ptr);
+return (0);
+}
