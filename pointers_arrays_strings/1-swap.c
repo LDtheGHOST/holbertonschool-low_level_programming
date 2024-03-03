28ddc9e96882077b0795b0552f98ad38170436b3
luis-david <luis-david.cuevas@epitech.eu>
2024-03-03 13:14:29 +0100

    28 fevirer

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,29 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strncat - concatene deux chaine suivie d'un n octet
- * @dest: chaine 1
- * @src: chaine 2
- * @n: nombre maximum de charactere
- * Return: dest
+ * swap_int - function that swaps the values of two integers.
+ * @a: integer
+ * @b: integer
+ * Return: void
  **/
-char *_strncat(char *dest, char *src, int n)
+void swap_int(int *a, int *b)
 {
-int i;
-int j;
+int aa;
 
-i = 0;
-while (dest[i] != '\0')
-{
-i++;
-}
-j = 0;
-while (src[j] != '\0' && j < n)
-{
-dest[i] = src[j];
-i++;
-j++;
-}
-dest[i] = '\0';
-return (dest);
+aa = *a;
+*a = *b;
+*b = aa;
 }
