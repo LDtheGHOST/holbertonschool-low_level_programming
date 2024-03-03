d2312ccb743901c729ad00c85f69d23213eee3cd
luis-david <luis-david.cuevas@epitech.eu>
2024-03-03 20:59:51 +0100

    29 fevirer

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strncpy - function that copies a string.
+ * @dest: char
+ * @src: char
+ * @n: integer
+ * Return: char
+ **/
+char *_strncpy(char *dest, char *src, int n)
+{
+char *p = dest;
+
+while (*src != '\0' && n > 0)
+{
+*p++ = *src++;
+n--;
+}
+
+while (n > 0)
+{
+*p++ = '\0';
+n--;
+}
+return (dest);
+}
---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: integer
+ * @n: integer
+ * Return: void
+ **/
+void reverse_array(int *a, int n)
+{
+int temp;
+int i;
+
+for (i = 0; i < n / 2; i++)
+{
+temp = a[i];
+a[i] = a[n - i - 1];
+a[n - i - 1] = temp;
+}
+}
