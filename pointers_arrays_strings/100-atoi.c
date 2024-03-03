131c696e8a2a8cb42096892ab1da294ba26c3990
luis-david <luis-david.cuevas@epitech.eu>
2024-03-03 23:44:00 +0100

    28 fevirer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -33,3 +33,29 @@ s++;
 }
 return (result);
 }
+
+int _atoi(char *s)
+{
+	int i = 1;
+	unsigned int num = 0;
+	int started = 0;
+
+	while (*s)
+	{
+		if (*s == '-')
+		{
+			i *= -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			num = num * 10 + (*s - '0');
+			started = 1;
+		}
+		else if (started)
+		{
+			break;
+		}
+		s++;
+	}
+	return (num * i);
+}
