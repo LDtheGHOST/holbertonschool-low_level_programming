75384b568aba64b95e0076495557d49c6a3bf738
luis-david <luis-david.cuevas@epitech.eu>
2024-03-08 21:42:02 +0100

    05.mars

--- pointers_arrays_strings/7-main.c
+++ pointers_arrays_strings/7-main.c
@@ -6,24 +6,6 @@
  * Return: Always 0.
  **/
 int main(void)
-{		
-char *str;
-
-str = "0123456789";
-puts_half(str);
-return (0);
-}
-
-{
-char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
-char *p;
-
-p = leet(s);
-printf("%s", p);
-printf("%s", s);
-return (0);
-}
-
 {
 char board[8][8] = {
 {'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
