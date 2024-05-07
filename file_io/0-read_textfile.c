3ac8210a93d98e9744a40d9df93152583a42a8d3
luis-david <luissmith@hotmail.fr>
2024-05-07 12:38:59 +0200

    projet

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,56 +1,48 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-/**
- * read_textfile - function that reads a text file and
- * prints it to the POSIX standard output.
- * @filename: name of the file
- * @letters: number of characters to read and print
- *
- * if the file can not be opened or read, return 0
- * if filename is NULL return 0
- * if write fails or does not write the expected amount of bytes, return 0
- *
- * Return: the actual number of letters it could read and print
- **/
 
+/**
+ * read_textfile - Reads a text file and prints it.
+ * @filename: Filename
+ * @letters: Letters can read and print
+ * Return: Count letters printed
+ **/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd = open(filename, O_RDONLY);
+int fd;
 char *buffer;
-ssize_t bytes_read, bytes_written;
+ssize_t bytes, bytes_w;
 
 if (filename == NULL)
-{
 return (0);
-}
+
+fd = open(filename, O_RDONLY);
 if (fd == -1)
-{
 return (0);
-}
+
 buffer = (char *)malloc(sizeof(char) * (letters + 1));
 if (buffer == NULL)
 {
 close(fd);
 return (0);
 }
-bytes_read = read(fd, buffer, letters);
+
+bytes = read(fd, buffer, letters);
+if (bytes == -1)
+{
 close(fd);
-
-if (bytes_read == -1)
-{
 free(buffer);
 return (0);
 }
-buffer[bytes_read] = '\0';
-bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-free(buffer);
 
-if (bytes_written == -1 || bytes_written != bytes_read)
+bytes_w = write(STDOUT_FILENO, buffer, bytes);
+if (bytes_w == -1 || bytes_w != bytes)
 {
+close(fd);
+free(buffer);
 return (0);
 }
-return (bytes_read);
+
+close(fd);
+free(buffer);
+return (bytes_w);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -1,13 +1,17 @@
-#ifndef FILE_IO_H
-#define FILE_IO_H
-
-#include <unistd.h>
-#include <fcntl.h>
+#ifndef IO_H
+#define IO_H
 #include <stddef.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int _putchar(char c);
+int _strlen(char *s);
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+void close_file(int fd);
+void _error(int status, char *v);
 #endif
